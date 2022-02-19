4f564e7bf7a7736fd5873a5d5eeb152fe6eb2ba5
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2022-02-19 13:50:35 -0500

    modified best_move to return bid alos

--- bots/template_bot.py
+++ bots/template_bot.py
@@ -56,7 +56,7 @@ class MyPlayer(Player):
         locAs = []
         for x in range(self.MAP_WIDTH):
             for y in range(self.MAP_HEIGHT):
-                if map[x][y].structure.team == self.team:
+                if map[x][y].structure and (map[x][y].structure.team == self.team):
                     locAs.append(map[x][y])
 
         return locAs 
@@ -66,39 +66,44 @@ class MyPlayer(Player):
     
     Params
         - locA: Tile
-        - budget: Float
     
     Returns
         - path: List[Tile]
+        - bid: float
         - reward: Float
     '''
-    def get_best_move(self, locA, budget):
+    def get_best_move(self, locA, player_info):
         max_reward = -math.inf
         max_path = None
+        max_bid = 0
+        sec_max_cost = -1
         for tower in self.get_cell_towers(self.map):
             path, reward, cost = self.get_reward(locA, tower)
-            if reward > max_reward and cost <= budget:
+            if reward > max_reward and cost <= player_info.money:
                 max_path = path
+                max_reward = reward
+                max_bid = (cost - sec_max_cost) if sec_max_cost == -1 else (player_info.money - cost)
+                sec_max_cost = cost
 
-        assert max_path is not None
-        return max_path, max_reward
+        return max_path, max_bid, max_reward
 
     '''
     Returns best paths to build given map and cost
     
     Params:
         - map: List[List[Tile]]
+        - player_info
         
     Returns:
         - paths: List[(List[Tile], reward)]
         - bid: Float
     '''
-    def get_best_path(self, map):
+    def get_best_path(self, map, player_info):
         best_path = None
         best_reward = -math.inf
         best_bid = None
         for locA in self.get_locAs(map):
-            path, bid, reward = self.get_best_move(locA)
+            path, bid, reward = self.get_best_move(locA, player_info)
             if path and reward > best_reward:
                 best_path = path
                 best_reward = reward
