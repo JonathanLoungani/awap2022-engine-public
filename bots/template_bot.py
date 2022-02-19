482cab6238b160b21d99e28ef5c6ba03ecc53fa4
eric-zhizu <zhou.eric.y@gmail.com>
2022-02-19 13:30:28 -0500

    Draft get_best_move

--- bots/template_bot.py
+++ bots/template_bot.py
@@ -1,5 +1,6 @@
 import sys
 
+import math
 import random
 
 from src.player import *
@@ -11,6 +12,7 @@ class MyPlayer(Player):
     def __init__(self):
         print("Init")
         self.turn = 0
+        self.map = None
 
         return
 
@@ -39,9 +41,30 @@ class MyPlayer(Player):
     Computes the estimated utility of building a path from location A to location B.
     '''
     def get_reward(self, locA, locB):
-        return
+        return [], 0
+
+    '''
+    Returns best path and estimated reward of path starting from location A
+    
+    Params
+        - locA: Tile
+    
+    Returns
+        - path: List[Tile]
+        - reward: Float
+    '''
+    def get_best_move(self, locA):
+        max_reward = -math.inf
+        max_path = None
+        for tower in self.get_cell_towers(self.map):
+            path, reward = self.get_reward(locA, tower)
+            if reward > max_reward:
+                max_path = path
+
+        assert max_path is not None
+        return max_path, max_reward
 
 
     def play_turn(self, turn_num, map, player_info):
-
+        self.map = map
         return
