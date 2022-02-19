94eafca3c346774f6240be8ba984ab810ee6712d
eric-zhizu <zhou.eric.y@gmail.com>
2022-02-19 13:40:44 -0500

    Modify get_best_move to incorporate budget

--- bots/template_bot.py
+++ bots/template_bot.py
@@ -41,24 +41,25 @@ class MyPlayer(Player):
     Computes the estimated utility of building a path from location A to location B.
     '''
     def get_reward(self, locA, locB):
-        return [], 0
+        return [], 0, 0
 
     '''
     Returns best path and estimated reward of path starting from location A
     
     Params
         - locA: Tile
+        - budget: Float
     
     Returns
         - path: List[Tile]
         - reward: Float
     '''
-    def get_best_move(self, locA):
+    def get_best_move(self, locA, budget):
         max_reward = -math.inf
         max_path = None
         for tower in self.get_cell_towers(self.map):
             path, reward, cost = self.get_reward(locA, tower)
-            if reward > max_reward:
+            if reward > max_reward and cost <= budget:
                 max_path = path
 
         assert max_path is not None
