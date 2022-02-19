7ea29be754c4fa0df08140329aeafe311575662a
jonathan loungani <jonathanloungani@jonathans-mbp.wifi.local.cmu.edu>
2022-02-19 13:09:18 -0500

    Outlined function signatures

--- bots/template_bot.py
+++ bots/template_bot.py
@@ -14,8 +14,31 @@ class MyPlayer(Player):
 
         return
 
+    '''
+    Calculate the minimum cost to build roads from locA to locB.
+    '''
+    def min_road_cost(self, map, locA, locB):
+        return
+
+    '''
+    Generator to yield optimal cell tower locations.
+    '''
+    def get_cell_towers(self, map):
+        return
+
+    '''
+    Get population at tile
+    '''
+    def get_population(self, tile):
+        return
+
+    '''
+    Computes the estimated utility of building a path from location A to location B.
+    '''
+    def get_reward(self, locA, locB):
+        return
+
 
     def play_turn(self, turn_num, map, player_info):
-
 
         return
